7e48b318c252b00ed521264b4690472029fa4772
Vadim <vadim@example.ru>
2016-11-15 15:26:48 +0200

    animate shit

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
 	var upDownButtons = $('div.upDown');
 
+	upDownButtons.each(function(index, el) {
+		this.Up = false;
+		this.Down = true;
+	});
 
-	for(var i = 0; i < upDownButtons.length; i++)
-	{
-		upDownButtons[i].Up = false;
-		upDownButtons[i].Down = true;
-	}
+	$(this).scroll(function(event) {
+		var distanceFromTop = 900;
+		if($(this).scrollTop() > distanceFromTop){
+			if(!$('#wrapper .goTopButton').hasClass('disp'))
+				$('#wrapper .goTopButton').toggleClass('disp');
+		}	
+		else
+			if($('#wrapper .goTopButton').hasClass('disp'))
+				$('#wrapper .goTopButton').toggleClass('disp');
+	});
 
-
+	var animationTime = 1200;
+	$('#wrapper .goTopButton').click(function(event) {
+		$('body').animate({scrollTop: 0}, animationTime);
+	});
 
 
 	$('div.upDown').each(function(){
@@ -42,10 +54,19 @@ $(document).ready(function() {
 
 	function slide( url, color, display, element){
 		 //element.css('background','url('+url+') no-repeat 97% 60%');
-		element.css('background-image','url('+url+')');
+		 element.css('background-image','url('+url+')');
 		//element.css('background-color', color);
 		element.next().css('display', display);
 	}
+
+	//navbar переход к секции
+	$('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 2000); // анимируем скроолинг к элементу scroll_el
+	}
+	    return false; // выключаем стандартное действие
+	});
 
 });
 
